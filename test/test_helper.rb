80a1a76ee7153611e7b973eaee05495f9eac07c8
rainmaize <baongo110397@gmail.com>
2019-05-22 09:49:34 +0700

    Chapter 3: Mostly static pages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,8 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+# frozen_string_literal: true
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
 
-  # Add more helper methods to be used by all tests here...
-end
+require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
