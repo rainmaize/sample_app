48455800b5fcb0326f6e8205703b36c21d823142
rainmaize <baongo110397@gmail.com>
2019-05-27 14:41:23 +0700

    Chapter 6: Modeling users

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,16 +9,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get help" do
     get static_pages_help_url
     assert_response :success
